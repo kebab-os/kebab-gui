bace77df72a059689114855383918c23872b3e52
7ames <123058@combertonvc.org>
2026-05-21 14:45:25 +0100

    Update windows.ps1

--- install/windows.ps1
+++ install/windows.ps1
@@ -1,7 +1,7 @@
 cd ~
 if (!(Test-Path .kebab)) { mkdir .kebab }
 cd .kebab
-curl.exe -L https://github.com/kebab-os/kebab-gui/archive/refs/heads/main.zip -o kebab-gui.zip
+curl.exe -L https://github.com/kebab-os/kebab-os/archive/refs/heads/main.zip -o kebab-gui.zip
 Expand-Archive -Path kebab-gui.zip -DestinationPath . -Force
 cd kebab-gui-main
 pip install pygame requests html2image
